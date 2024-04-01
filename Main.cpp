ceb78454ee8b54addc1f10636afdfc47126bc451
Chase Horton <143553969+cohorton@users.noreply.github.com>
2024-04-01 11:52:21 -0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,53 @@
+// Your name
+// Your partner's name (if none, delete this line)
+
+#include "MazeGenerator.hpp"
+#include "Labyrinth.hpp"
+#include <iostream>
+#include <string>
+
+/* Change this constant to contain your name.
+ *
+ * WARNING: Once you've set set this constant and started exploring your maze,
+ * do NOT edit the value of kYourName. Changing kYourName will change which
+ * maze you get back, which might invalidate all your hard work!
+ */
+const std::string kYourName = "Chase";
+
+/* Change these constants to contain the paths out of your mazes. */
+const std::string kPathOutOfNormalMaze = "WWSWSEWNEESSWWEENNSES";
+const std::string kPathOutOfTwistyMaze = "SWENSWSEWNWWSWW";
+
+/* Don't change anything else in this file! */
+int main() {
+    /* Generate the maze.
+     *
+     * Note: Don't set a breakpoint on this line. Otherwise, you'll see startLocation before
+     * it's been initialized.
+     */
+    MazeCell* startLocation = mazeFor(kYourName);
+    
+    /* Set a breakpoint here to explore your maze! */
+    if (isPathToFreedom(startLocation, kPathOutOfNormalMaze)) {
+        std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
+    } else {
+        std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
+    }
+    
+    
+    /* Generate the twisty maze.
+     *
+     * Note: Don't set a breakpoint on this line. Otherwise, you'll see twistyStartLocation before
+     * it's been initialized.
+     */
+    MazeCell* twistyStartLocation = twistyMazeFor(kYourName);
+    
+    /* Set a breakpoint here to explore your twisty maze! */
+    if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze)) {
+        std::cout << "Congratulations! You've found a way out of your twisty labyrinth." << std::endl;
+    } else {
+        std::cout << "Sorry, but you're still stuck in your twisty labyrinth." << std::endl;
+    }
+    
+    return 0;
+}
